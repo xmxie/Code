0bf8e760699de3779f8f132e5975fb29dd729ccb
Kirhhoff <37506148+Kirhhoff@users.noreply.github.com>
2018-07-13 12:42:57 +0800

    小的改动

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@ int main() {
 	string* classifierPathName;//各级强分类器中的弱分类器权重路径名数组
 	Sample* samples;//样本数组
 
-	samples = GetSamples(samplePathName);//获取样本
+	samples = GetSamples(posPathName, negPathName);//获取样本
 	CalIntegralDiagrams(samples);//计算样本积分图
 	Train(samples);//训练样本 期间会更新样本权重以及各强分类器中的弱分类器的权重
 }
